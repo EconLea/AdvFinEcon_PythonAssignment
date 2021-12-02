5817bf48cc3f46c561ac56a5fafa281cf78888c1
Lea Paoli <lea.paoli@gmx.de>
2021-12-02 12:48:57 +0100

    :running: work in progress

--- Code_workingFile.py
+++ Code_workingFile.py
@@ -6,8 +6,6 @@
 # Purpose:               XXXXXX 
 #************************************************************************************************
 
-##ask the user for path: 
-
 #***********************IMPORT MODULES***********************
 
 # Installation of packages [Use the command line, not the Python shell]
@@ -44,9 +42,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import StratifiedKFold #as opposed to KFold
 
+import itertools
 
 import matplotlib.pyplot as plt
 import seaborn as sn 
+
 
 #***********************PARAMETRIZATION***********************
 print("***********************PARAMETRIZATION***********************")
@@ -67,10 +67,9 @@ print("number_of_splits is set to " + str(number_of_splits))
 #***********************FUNCTIONS***********************
 #DELETE PLEASE LOOK HERE:
 # these functions are needed to plot the trees-graphs 
-def Ey_x(x):
-    return 1/3*(np.sin(5*x[0])*np.sqrt(x[1])*np.exp(-(x[1]-0.5)**2)) #DELETE: PLEASE LOOK HERE: WTF is this for?
 
-def surface_scatter_plot(X,y,f, xlo=0., xhi=1., ngrid=50, width=860, height=700, f0=Ey_x, show_f0=False): #PLEASE LOOK HERE: f0=Ey_x?!
+
+def surface_scatter_plot(X,y,f, ngrid=50, width=860, height=700): 
     scatter = go.Scatter3d(x=X[:,0],y=X[:,1],z=y,
                            mode='markers',
                            marker=dict(size=2, opacity=0.3)
@@ -78,19 +77,12 @@ def surface_scatter_plot(X,y,f, xlo=0., xhi=1., ngrid=50, width=860, height=700,
 
     xgrid = np.linspace(xlo,xhi,ngrid)
     ey = np.zeros((len(xgrid),len(xgrid)))
-    ey0 = np.zeros((len(xgrid),len(xgrid)))
     colorscale = [[0, colors[0]], [1, colors[2]]]
     for i in range(len(xgrid)):
         for j in range(len(xgrid)):
             ey[j,i] = f([xgrid[i],xgrid[j]])
-            ey0[j,i]= f0([xgrid[i],xgrid[j]])
     
     surface = go.Surface(x=xgrid, y=xgrid, z=ey, colorscale=colorscale, opacity=1.0)
-    if (show_f0):
-        surface0 = go.Surface(x=xgrid, y=xgrid, z=ey0, opacity=0.8, colorscale=colorscale)
-        layers = [scatter, surface, surface0]
-    else:
-        layers = [scatter, surface]
     
     fig = go.FigureWidget(
         data=layers,
@@ -108,6 +100,25 @@ def surface_scatter_plot(X,y,f, xlo=0., xhi=1., ngrid=50, width=860, height=700,
     )
     return fig
 
+
+def plot_roc(mod, X, y):
+    # predicted_probs is an N x 2 array, where N is number of observations and 2 is number of classes
+    predicted_probs = mod.predict_proba(X_test)
+
+    # keep the second column, for label=1
+    predicted_prob1 = predicted_probs[:, 1]
+
+    fpr, tpr, _ = metrics.roc_curve(y_test, predicted_prob1)
+
+    # Plot ROC curve
+    fig, ax = plt.subplots()
+    ax.plot([0, 1], [0, 1], "k--")
+    ax.plot(fpr, tpr)
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+    ax.set_title("ROC Curve")
+
+    return fig, ax
 
 #***********************PREPATORY WORK***********************
 print("***********************PREPATORY WORK***********************")
@@ -250,9 +261,9 @@ print("number of obs. in training data set is " + str(len(X_train))) #should equ
 # HOWEVER: I cannot run it - neither there nor on my local python. The TA managed to do it if I remember correctly 
 
 
-fitted_tree = tree.DecisionTreeClassifier(max_depth=3).fit(Xsim,ysim)
-fig=surface_scatter_plot(Xsim, ysim, lambda x: fitted_tree.predict([x]), show_f0=True) #lamdba refers to the "anonymous" lambda funtion
-fig.show() 
+# fitted_tree = tree.DecisionTreeClassifier(max_depth=3).fit(Xsim,ysim)
+# fig=surface_scatter_plot(Xsim, ysim, lambda x: fitted_tree.predict([x])) #lamdba refers to the "anonymous" lambda funtion
+# fig.show() 
 
 #DELETE: LOOK HERE: THIS IS REMARK 1 -> I think this needs to be integrated into your part somewhere 
 # The terminology 'logistic regression with LASSO regularization is a bit misleading. I should have said 'l1' regularization, 
@@ -279,93 +290,112 @@ fig.show()
 
 
 # #--- MODEL4: random forest
-# # DELETE: https://www.youtube.com/watch?v=ok2s1vV9XW0
-# for i in np.arange(1, 5): # number of trees in the forest
-#     print("number of trees is equal to "+ str(i))
-#     model4=RandomForestClassifier(n_estimators=i)# use Classifier instead of Regression, as this is a classification problem!
-#     #DELETE: https://machinelearningmastery.com/classification-versus-regression-in-machine-learning/
-#     model4.fit(X_train, y_train)
-#     print("model score for " + str(i) + "trees: " + str(model4.score(X_test, y_test)))
-#     model4.feature_importances_
 
+# DELETE: https://www.youtube.com/watch?v=ok2s1vV9XW0 & #DELETE: https://machinelearningmastery.com/classification-versus-regression-in-machine-learning/
 
-#     fig=surface_scatter_plot(X_train,y_train,lambda x: forest.predict([x]), show_f0=True)
-#     fig.show()
+for i in np.arange(1, 5): # number of trees in the forest
+     print("number of trees is equal to "+ str(i))
+     model4=RandomForestClassifier(n_estimators=i)# use Classifier instead of Regression, as this is a classification problem!
+     model4.fit(X_train, y_train)
+     print("model score for " + str(i) + " trees: " + str(model4.score(X_test, y_test)))
+     print("Return the feature importances for " + str(i) + " trees: ")
+     print(model4.feature_importances_)
+     print("Return confusion_matrix for " + str(i) + " trees: ")
+     print(metrics.confusion_matrix(y_test, model4.predict(X_test)))
+     fig=surface_scatter_plot(X_train,y_train,lambda x: model4.predict(X_test))
+     fig.show()
 
 # drawing single decision graph no longer possible 
 # Feature importance is the average MSE decrease caused by splits on each feature.
-# If a given feature has greater importance, the trees split on that feature more often and/or splitting on that feature resulted in larger MSE decreases.
+# If a given feature has greater importance, the trees split on that feature more often and/or splitting on that feature 
+# resulted in larger MSE decreases.
 
 
 
 ## MODEL5: neural networks.
 # Experiment with different numbers of hidden layers, and neurons for each layers, not necessarily using cross-validation
-from sklearn import neural_network
-model5 = neural_network.MLPClassifier((6,), activation="logistic", verbose=True, solver="lbfgs", alpha=0.0)
+
+hiddenlayers = 2
+nrneurons_1stlayer = np.array([100, 500, 1000]) 
+nrneurons_2ndlayer = np.array([100, 500, 1000]) 
+# The ith element represents the number of neurons in the ith hidden layer.
+
+
 #‘lbfgs’ is an optimizer in the family of quasi-Newton methods.
 # Alpha is a parameter for regularization term, aka penalty term, that combats overfitting by constraining the size of the weights.
 # https://scikit-learn.org/stable/auto_examples/neural_networks/plot_mlp_alpha.html
-model5.fit(X_test,y_test)
 
-fig=surface_scatter_plot(Xsim,ysim,lambda x: nn.predict([x]), show_f0=True)
-fig.show()
+for i, j in itertools.product(nrneurons_1stlayer, nrneurons_2ndlayer): #cartesian product 
+    print("we  look at the following combinations of neurons per layer: " + str(i) + "+" + str(j)
 
-# two hidden layers, with N1=30 and N2=20
-nn_model = neural_network.MLPClassifier((30, 20))
-nn_model.fit(X_test, y_test)
+for i, j in itertools.product(nrneurons_1stlayer, nrneurons_2ndlayer): 
+     print("number of hiddenlayers is equal to "+ str(hiddenlayers))
+     print("number of neurons per layer is equal to "+ str(i) + "and "+ str(j) + "respect.")
+     model5=neural_network.MLPClassifier((i, j), activation="logistic", verbose=True, solver="lbfgs", alpha=0.0)
+     model5.fit(X_train, y_train)
+     mse_model5 = metrics.mean_squared_error(y, model5.predict(X))
+     print(str(mse_model5))
+
+
+hiddenlayers = 1
+nrneurons_1stlayer = np.array([100, 500, 1000]) 
+
+for i in nrneurons_1stlayer: 
+     print("number of hiddenlayers is equal to "+ str(hiddenlayers))
+     print("number of neurons per layer is equal to "+ str(i))
+     model5=neural_network.MLPClassifier((i), activation="logistic", verbose=True, solver="lbfgs", alpha=0.0)
+     model5.fit(X_train, y_train)
+     mse_model5 = metrics.mean_squared_error(y, model5.predict(X))
+     print(str(mse_model5))
+
+# DELETE: THIS FUNCTION NEEDS DEBUGGING
+#fig=surface_scatter_plot(Xsim,ysim,lambda x: nn.predict([x]), show_f0=True)
+#fig.show()
+
+
 #https://stackoverflow.com/questions/62658215/convergencewarning-lbfgs-failed-to-converge-status-1-stop-total-no-of-iter
 
-#ax = var_scatter(df)
-#scatter_model(nn_model, X, ax=ax)
-
-mse_nn = metrics.mean_squared_error(y, model5.predict(X))
-
-
 # Standardize 
-nn_scaled_model = pipeline.make_pipeline(
+print("STANDARDIZATION")
+model5_scaled = pipeline.make_pipeline(
     preprocessing.StandardScaler(),  # this will do the input scaling
     neural_network.MLPClassifier((30, 20)) 
 )
 
-# We can now use `model` like we have used our other models all along
-# Call fit
-nn_scaled_model.fit(X_test, y_test)
+hiddenlayers = 1
+nrneurons_1stlayer = np.array([100, 500, 1000]) 
 
-# Call predict
-mse_nn_scaled = metrics.mean_squared_error(y, nn_scaled_model.predict(X))
+for i in nrneurons_1stlayer: 
+     print("number of hiddenlayers is equal to "+ str(hiddenlayers))
+     print("number of neurons per layer is equal to "+ str(i))
+     model5_scaled=pipeline.make_pipeline(
+        preprocessing.StandardScaler(),  # this will do the input scaling
+        neural_network.MLPClassifier((i), activation="logistic", verbose=True, solver="lbfgs", alpha=0.0)
+        )
+     model5_scaled.fit(X_train, y_train)
+     mse_model5_scaled = metrics.mean_squared_error(y, model5_scaled.predict(X))
+     print(str(mse_model5_scaled))
+
+
 #mse_nn / metrics.mean_squared_error(y, lr_model.predict(X)) NOT YET POSSIBLE AS LINEAR MODEL IS MISSING
 
-print(f"Unscaled mse {mse_nn}")
-print(f"Scaled mse {mse_nn_scaled}")
+print(f"Unscaled mse {mse_model5}")
+print(f"Scaled mse {mse_model5_scaled}")
 
 #ax = var_scatter(df)
 #scatter_model(nn_scaled_model, X, ax=ax)
+
+#-------- Plot learning curves 
+#https://www.dataquest.io/blog/learning-curves-machine-learning/
+
 
 #-------- ROC curves
 #Plot the ROC curves for the best versions of your models and compute the AUROC. 
 #DELETE: https://www.youtube.com/watch?v=gJo0uNL-5Qw
 #See classification file
 
-def plot_roc(mod, X, y):
-    # predicted_probs is an N x 2 array, where N is number of observations
-    # and 2 is number of classes
-    predicted_probs = mod.predict_proba(X_test)
-
-    # keep the second column, for label=1
-    predicted_prob1 = predicted_probs[:, 1]
-
-    fpr, tpr, _ = metrics.roc_curve(y_test, predicted_prob1)
-
-    # Plot ROC curve
-    fig, ax = plt.subplots()
-    ax.plot([0, 1], [0, 1], "k--")
-    ax.plot(fpr, tpr)
-    ax.set_xlabel("False Positive Rate")
-    ax.set_ylabel("True Positive Rate")
-    ax.set_title("ROC Curve")
-
-plot_roc(logistic_age_model, X_test, y_test)
-
+roc_fig=plot_roc(model5_scaled, X_test, y_test)
+plt.show()
 
 #AUC Value (AUROC?!)
 predicted_prob1 = logistic_age_model.predict_proba(X)[:, 1]
@@ -381,10 +411,12 @@ def get_score(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     return model.score(x_test)
 
-get_score(LogisticRegression, X_train, X_test, y_train, y_test)
+get_score(model5_scaled, X_train, X_test, y_train, y_test)
 
 for train_index, test_index in kf.split(df_final):
-    X_train, X_test, y_train, y_test = df_final[train_index], df_final[test_index], df_final[train]  
+    X_train, X_test, y_train, y_test = df_final[train_index], df_final[test_index], df_final[train] 
+
+
 #-------- Confusion Matrices Comparison
 #Plot the true value of y against the prediced value 
 y_predicted = model.predict(X_test)
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ ctrl+f for: "DELETE:" in the end
 
 Fragen: 
 - was ist mit der surface plotting funktion?
+- wie Anzahl Layers/Neutrons auswählen
+- automatische Modellauswahl nach Vgl der Parameter?
